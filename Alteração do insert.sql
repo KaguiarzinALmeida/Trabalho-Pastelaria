d21ef2cb5232e276efc7747f6d27efc9ff6d8a21
KaguiarzinALmeida <aguiark71@gmail.com>
2024-05-31 21:15:36 -0300

    Alteração nos inserts

---
+++ Alteração do insert.sql
@@ -0,0 +1,118 @@
+USE stop_pastelaria;
+
+
+INSERT INTO clientes (nome_completo, nome_a_ser_chamado, cpf, data_nascimento, telefone, email, bairro, cidade, estado)
+VALUES 
+( ' Ana Santos ' , ' Ana ' , ' 111.222.333-44 ' , '10 /03/1988 ' , ' (11) 1111-2222 ' , ' ana@example.com ' , ' Vila Madalena ' , ' São Paulo ' , ' SP ' ),
+( ' Carlos Lima ' , ' Carlos ' , ' 555.666.777-88 ' , '22 /07/1995 ' , ' (11)3333-4444 ' , ' carlos@example.com ' , ' Pinheiros ' , ' São Paulo ' , ' SP ' ),
+( ' Fernanda Souza ' , ' Fernanda ' , ' 999.888.777-66 ' , ' 1980-11-05 ' , ' (11) 5555-6666 ' , ' fernanda@example.com ' , ' Moema ' , ' São Paulo ' , ' SP ' ),
+( ' Pedro Pereira ' , ' Pedro ' , ' 123.456.789-01 ' , '30 /01/1992 ' , ' (11)7777-8888 ' , ' pedro@example.com ' , ' Itaim Bibi ' , ' São Paulo ' , ' SP ' ),
+( ' Larissa Silva ' , ' Larissa ' , ' 987.654.321-09 ' , ' 1987-12-15 ' , ' (11)9999-0000 ' , ' larissa@example.com ' , ' Brooklin ' , ' São Paulo ' , ' SP ' ),
+( ' Marcos Oliveira ' , ' Marcos ' , ' 456.789.123-00 ' , '18 /04-1982 ' , ' (11) 2222-3333 ' , ' marcos@example.com ' , ' Campo Belo ' , ' São Paulo ' , ' SP ' ),
+( ' Juliana Costa ' , ' Juliana ' , ' 654.321.987-01 ' , ' 20/09/1990 ' , ' (11) 8888-9999 ' , ' juliana@example.com ' , ' Perdizes ' , ' São Paulo ' , ' SP ' ),
+( ' Lucas Santos ' , ' Lucas ' , ' 987.654.321-00 ' , '25/06/1985 ' , ' ( 11)3333-5555 ' , ' lucas@example.com ' , ' Morumbi ' , ' São Paulo ' , ' SP ' ),
+( ' Camila Rocha ' , ' Camila ' , ' 789.123.456-02 ' , '14 /02/1998 ' , ' (11) 6666-7777 ' , ' camila@example.com ' , ' Jabaquara ' , ' São Paulo ' , ' SP ' ),
+( ' Gabriel Almeida ' , ' Gabriel ' , ' 321.654.987-03 ' , ' 1989-10-12 ' , ' (11) 0000-1111 ' , 'gabriel@example.com ' , ' Tatuapé ' , ' São Paulo ' , ' SP ' );
+
+-- Inserir clientes com menos de 18 anos
+INSERT INTO clientes (nome_completo, nome_a_ser_chamado, cpf, data_nascimento, telefone, email, bairro, cidade, estado)
+VALUES 
+( ' Ana Silva ' , ' Ana ' , ' 222.333.444-55 ' , ' 2008-02-28 ' , ' (11) 1143-2222 ' , ' ana@example.com ' , ' Vila Madalena ' , ' São Paulo ' , ' SP ' ),
+( ' Pedro Oliveira ' , ' Pedro ' , ' 333.444.555-66 ' , ' 2010-05-10 ' , ' (11) 2652-3333 ' , ' pedro@example.com ' , ' Pinheiros ' , ' São Paulo ' , ' SP ' ),
+( ' Mariana Santos ' , ' Mariana ' , ' 444.555.666-77 ' , '15 -09-2005 ' , ' (11)3553-4444 ' , ' mariana@example.com ' , ' Moema ' , ' São Paulo ' , ' SP ' );
+
+
+INSERT INTO pasteis (nome, tamanho, preço, categoria)
+VALUES 
+( ' Pastel de Carne ' , ' Médio ' , 5.50 , ' Salgado ' ) ,
+( ' Pastel de Queijo ' , ' Pequeno ' , 4.00 , ' Salgado ' ) ,
+( ' Pastel de Frango ' , ' Grande ' , 6.50 , ' Salgado ' ) ,
+( ' Pastel de Calabresa ' , ' Médio ' , 6.00 , ' Salgado ' ) ,
+( ' Pastel de Palmito ' , ' Pequeno ' , 4.50 , ' Salgado ' ) ,
+( ' Pastel de Camarão ' , ' Grande ' , 7.00 , ' Salgado ' ) ,
+( ' Pastel de Chocolate ' , ' Médio ' , 5.00 , ' Doce ' ) ,
+( ' Pastel de Banana ' , ' Pequeno ' , 3.50 , ' Doce ' ) ,
+( ' Pastel de Morango ' , ' Grande ' , 6.00 , ' Doce ' ) ,
+( ' Pastel de Leite Condensado ' , ' Médio ' , 4.50 , ' Doce ' ) ;
+
+-- Inserir pastéis veganos
+INSERT INTO pasteis (nome, tamanho, preço, categoria)
+VALUES
+( ' Pastel de Abóbora ' , ' Médio ' , 5.50 , ' Vegano ' ) ,
+( ' Pastel de Quinoa com Vegetais ' , ' Grande ' , 8.00 , ' Vegano ' ) ,
+( ' Pastel de Batata Doce com Lentilha ' , ' Pequeno ' , 4.50 , ' Vegano ' ) ,
+( ' Pastel de Couve-Flor com Grão-de- Bico ' , ' Médio ' , 6.00 , ' Vegano ' ) ,
+( ' Pastel de Berinjela com Pimentão ' , ' Grande ' , 7.00 , ' Vegano ' ) ;
+
+
+
+INSERT INTO recheios (nome)
+VALUES 
+( ' Carne ' ),
+( ' Queijo ' ),
+( ' Frango ' ),
+( ' Calabresa ' ),
+( ' Palmito ' ),
+( ' Camarão ' ),
+( ' Chocolate ' ),
+( ' Banana ' ),
+( ' Morango ' ),
+( ' Leite Condensado ' );
+
+
+INSERT INTO recheios (nome)
+VALUES
+( ' Abóbora ' ),
+( ' Quinoa ' ),
+( ' Vegetais ' ),
+( ' Batata Doce ' ),
+( ' Lentilha ' ),
+( ' Couve-Flor ' ),
+( ' Grão-de-Bico ' ),
+( ' Berinjela ' ),
+( ' Pimentão ' );
+
+
+
+
+INSERT INTO pedidos (id_cliente, data_pedido, forma_pagamento)
+VALUES 
+( 3 , ' 2024-05-24 ' , ' Cartão de Débito ' ),
+( 4 , ' 2024-05-23 ' , ' Dinheiro ' ),
+( 5 , ' 2024-05-22 ' , ' Dinheiro ' ),
+( 6 , ' 2024-05-21 ' , ' Cartão de Crédito ' ),
+( 7 , ' 2024-05-20 ' , ' Cartão de Crédito ' ),
+( 8 , ' 2024-05-19 ' , ' Dinheiro ' ),
+( 9 , ' 2024-05-18 ' , ' Cartão de Crédito ' ),
+( 10 , ' 2024-05-17 ' , ' Dinheiro ' ),
+( 1 , ' 2024-05-16 ' , ' Cartão de Débito ' ),
+( 2 , ' 2024-05-15 ' , ' Dinheiro ' );
+
+INSERT INTO pasteis_recheios (id_pastel, id_recheio)
+VALUES
+(1, 1), 
+(1, 2), 
+(3, 3), 
+(5, 5), 
+(5, 2); 
+
+INSERT INTO produtos_vendidos (id_pedido, id_produto, quantidade)
+VALUES
+(1, 1, 2), 
+(2, 3, 1), 
+(3, 5, 3),
+(4, 10, 2),
+(5, 15, 1),
+(6, 11, 4),
+(7, 13, 5); 
+
+
+SELECT  *  FROM clientes;
+
+SELECT  *  FROM recheios;
+
+SELECT  *  FROM produtos_vendidos;
+
+SELECT  *  FROM pedidos;
+
+SELECT  *  FROM pasteis;
